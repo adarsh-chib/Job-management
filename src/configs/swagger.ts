db955ecb1c6b4303c7a719ceae768649af3bc753
adarsh-chib <adarshchib1999@gmail.com>
2026-04-16 18:39:27 +0530

    impliment swagger

---
+++ src/configs/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Job Management API",
+            version: "1.0.0",
+            description: "API documentation for Job Management System",
+        },
+        servers: [
+            {
+                url: "https://job-management-e4z4.onrender.com",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.ts"], // path to your route files
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
